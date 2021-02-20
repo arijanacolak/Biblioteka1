24d01a8da667c3fc967fd1dae94f130e3e79f893
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2021-02-20 22:36:42 +0100

    dodat prozor za detaljni prikaz podataka o korisniku i isti je povezan sa listom

---
+++ src/ba/unsa/etf/icr/detaljniPrikazPodataka.java
@@ -0,0 +1,38 @@
+package ba.unsa.etf.icr;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class detaljniPrikazPodataka {
+    public TextField fldIme;
+    public TextField fldPrezime;
+    public TextField fldImeRoditelja;
+    public TextField fldAdresaPrebivalista;
+    public TextField fldJmbg;
+    public DatePicker dateDatumRodjenja;
+    public ComboBox comboMjestoRodjenja;
+    public ComboBox comboMjestoPrebivalista;
+    public TextField fldPostanskiBroj;
+    public Button btnNazad;
+
+    public detaljniPrikazPodataka(){
+
+    }
+
+    public void initialize(){
+        ObservableList<String> listaGradova = FXCollections.observableArrayList();
+        listaGradova.addAll("Sarajevo", "Mostar", "Bihać", "Banja Luka");
+
+        comboMjestoPrebivalista.setItems(listaGradova);
+        comboMjestoRodjenja.setItems(listaGradova);
+    }
+    public void nazadAction(ActionEvent actionEvent) {
+        ((Stage) btnNazad.getScene().getWindow()).close();
+    }
+}
--- src/ba/unsa/etf/icr/obrisiKorisnikaController.java
+++ src/ba/unsa/etf/icr/obrisiKorisnikaController.java
@@ -3,11 +3,18 @@ package ba.unsa.etf.icr;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Optional;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class obrisiKorisnikaController {
     public Button btnNazad;
@@ -29,14 +36,46 @@ public class obrisiKorisnikaController {
 
         ObservableList<Korisnik> listaKnjiga = FXCollections.observableArrayList();
 
+
+
         listaKnjiga.addAll(tableKorisnici.getItems());
+
     }
 
     public void nazadAction(ActionEvent actionEvent) {
         ((Stage) btnNazad.getScene().getWindow()).close();
     }
 
-    public void detaljniPrikazPodatakaAction(ActionEvent actionEvent) {
+    public void detaljniPrikazPodatakaAction(ActionEvent actionEvent) throws IOException {
+        if(tableKorisnici.getSelectionModel().getSelectedItem() != null) {
+            Stage myStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/detaljniPrikazPodataka.fxml"));
+            Parent root = loader.load();
+            detaljniPrikazPodataka podaci = loader.getController();
+
+            podaci.fldIme.setText(tableKorisnici.getSelectionModel().getSelectedItem().getIme());
+            podaci.fldPrezime.setText(tableKorisnici.getSelectionModel().getSelectedItem().getPrezime());
+            podaci.fldImeRoditelja.setText(tableKorisnici.getSelectionModel().getSelectedItem().getImeRoditelja());
+            podaci.fldAdresaPrebivalista.setText(tableKorisnici.getSelectionModel().getSelectedItem().getAdresaPrebivalista());
+            podaci.fldJmbg.setText(tableKorisnici.getSelectionModel().getSelectedItem().getJmbg().toString());
+            podaci.fldPostanskiBroj.setText(tableKorisnici.getSelectionModel().getSelectedItem().getPostanskiBroj().toString());
+            podaci.dateDatumRodjenja.setValue(tableKorisnici.getSelectionModel().getSelectedItem().getDatumRodjenja());
+            podaci.comboMjestoRodjenja.setValue(tableKorisnici.getSelectionModel().getSelectedItem().getMjestoRodjenja());
+            podaci.comboMjestoPrebivalista.setValue(tableKorisnici.getSelectionModel().getSelectedItem().getMjestoPrebivalista());
+
+            myStage.setTitle("Podaci o korisniku");
+            myStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            myStage.setResizable(false);
+            myStage.showAndWait();
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Niste selektovali knjigu.");
+            alert.setContentText("Da biste otvorili ovaj prozor, potrebno je da selektujete željenu knjigu!");
+            alert.showAndWait();
+        }
+
     }
 
     //potrebno dodati uslov da je selektovan korisnik iz tabele
