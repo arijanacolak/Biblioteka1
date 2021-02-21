e09a306adcde5fa079081f84fc073108b4b2008e
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2021-02-21 23:04:48 +0100

    dodata validacija

--- src/ba/unsa/etf/icr/registracijaKnjigeController.java
+++ src/ba/unsa/etf/icr/registracijaKnjigeController.java
@@ -83,26 +83,72 @@ public class registracijaKnjigeController {
     }*/
 
     public void actionOkButton(ActionEvent actionEvent) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Potvrdi");
-        alert.setHeaderText("Potvrda o dodavanju nove knjige.");
-        alert.setContentText("Jeste li sigurni da želite dodati novu knjigu?");
+        if(!knjigaField.getText().equals("") && knjigaField.getText() != null &&
+                !autorField.getText().equals("") && autorField.getText() != null &&
+                !ISBNField.getText().equals("") && ISBNField.getText() != null &&
+                !izdavacField.getText().equals("") && izdavacField.getText() != null &&
+                !izdanjeField.getText().equals("") && izdanjeField.getText() != null &&
+        kategorijaComboBox.getValue() != null && jezikComboBox.getValue() != null &&
+                fontComboBox.getValue() != null) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Potvrdi");
+            alert.setHeaderText("Potvrda o dodavanju nove knjige.");
+            alert.setContentText("Jeste li sigurni da želite dodati novu knjigu?");
 
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
-            Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
-            alert1.setTitle("Potvrdi");
-            alert1.setHeaderText("Knjiga uspješno dodana.");
-            Optional<ButtonType> result1 = alert1.showAndWait();
+            Optional<ButtonType> result = alert.showAndWait();
             if (result.get() == ButtonType.OK) {
+                knjigaField.setStyle("-fx-background-color: lightgreen");
+                autorField.setStyle("-fx-background-color: lightgreen");
+                ISBNField.setStyle("-fx-background-color: lightgreen");
+                izdavacField.setStyle("-fx-background-color: lightgreen");
+                izdanjeField.setStyle("-fx-background-color: lightgreen");
+                kategorijaComboBox.setStyle("-fx-background-color: lightgreen");
+                jezikComboBox.setStyle("-fx-background-color: lightgreen");
+                fontComboBox.setStyle("-fx-background-color: lightgreen");
+
+                Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+                alert1.setTitle("Potvrdi");
+                alert1.setHeaderText("Knjiga uspješno dodana.");
+                Optional<ButtonType> result1 = alert1.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    alert.close();
+                }
+                alert.close();
+            } else {
                 alert.close();
             }
-            alert.close();
-        } else {
-            alert.close();
-        }
-        //((Stage) OkButton.getScene().getWindow()).close();
+            //((Stage) OkButton.getScene().getWindow()).close();
+        } else{
+            if(knjigaField.getText().equals("") || knjigaField.getText() == null) knjigaField.setStyle("-fx-background-color: lightpink");
+            else knjigaField.setStyle("-fx-background-color: lightgreen");
 
+            if(autorField.getText().equals("") || autorField.getText() == null) autorField.setStyle("-fx-background-color: lightpink");
+            else autorField.setStyle("-fx-background-color: lightgreen");
+
+            if(ISBNField.getText().equals("") || ISBNField.getText() == null) ISBNField.setStyle("-fx-background-color: lightpink");
+            else ISBNField.setStyle("-fx-background-color: lightgreen");
+
+            if(izdavacField.getText().equals("") || izdavacField.getText() == null) izdavacField.setStyle("-fx-background-color: lightpink");
+            else izdavacField.setStyle("-fx-background-color: lightgreen");
+
+            if(izdanjeField.getText().equals("") || izdanjeField.getText() == null) izdanjeField.setStyle("-fx-background-color: lightpink");
+            else izdanjeField.setStyle("-fx-background-color: lightgreen");
+
+            if(!kategorijaComboBox.isPressed() || kategorijaComboBox.getValue() == null) kategorijaComboBox.setStyle("-fx-background-color: lightpink");
+            else kategorijaComboBox.setStyle("-fx-background-color: lightgreen");
+
+            if(!jezikComboBox.isPressed() || jezikComboBox.getValue() == null) jezikComboBox.setStyle("-fx-background-color: lightpink");
+            else jezikComboBox.setStyle("-fx-background-color: lightgreen");
+
+            if(!fontComboBox.isPressed() || fontComboBox.getValue() == null) fontComboBox.setStyle("-fx-background-color: lightpink");
+            else fontComboBox.setStyle("-fx-background-color: lightgreen");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Nije moguće dodati knjigu!");
+            alert.setContentText("Niste popunili sva input polja!");
+
+            alert.showAndWait();
+        }
     }
 
     public void actionCacelButton(ActionEvent actionEvent) {
--- src/ba/unsa/etf/icr/registracijaKorisnikaController.java
+++ src/ba/unsa/etf/icr/registracijaKorisnikaController.java
@@ -6,6 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
+import java.util.Date;
 import java.util.Optional;
 
 public class registracijaKorisnikaController {
@@ -29,23 +32,77 @@ public class registracijaKorisnikaController {
         placeOfBirth.setItems(gradovi);
     }
     public void actionOkButton(ActionEvent actionEvent) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Potvrdi");
-        alert.setHeaderText("Potvrda o dodavanju novog korisnika.");
-        alert.setContentText("Jeste li sigurni da želite dodati ovog korisnika?");
+        if(nameField.getText() != null && !nameField.getText().equals("")&& surnameField.getText() != null && !surnameField.getText().equals("") && parentNameField.getText() != null && !parentNameField.getText().equals("") &&
+           jmbgField.getText() != null && !jmbgField.getText().equals("") && addressField.getText() != null && !addressField.getText().equals("") && addressPlace.getValue() != null && dateField.getValue().isBefore(ChronoLocalDate.from(LocalDateTime.now())) &&
+           placeOfBirth.getValue() != null && addressPlace.getValue() != null && postalNumberField.getText() != null && !postalNumberField.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Potvrdi");
+            alert.setHeaderText("Potvrda o dodavanju novog korisnika.");
+            alert.setContentText("Jeste li sigurni da želite dodati ovog korisnika?");
 
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
-            Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
-            alert1.setTitle("Potvrdi");
-            alert1.setHeaderText("Korisnik uspješno dodan.");
-            Optional<ButtonType> result1 = alert1.showAndWait();
+            Optional<ButtonType> result = alert.showAndWait();
             if (result.get() == ButtonType.OK) {
+                nameField.setStyle("-fx-background-color: lightgreen");
+                surnameField.setStyle("-fx-background-color: lightgreen");
+                parentNameField.setStyle("-fx-background-color: lightgreen");
+                jmbgField.setStyle("-fx-background-color: lightgreen");
+                addressField.setStyle("-fx-background-color: lightgreen");
+                addressPlace.setStyle("-fx-background-color: lightgreen");
+                dateField.setStyle("-fx-background-color: lightgreen");
+                placeOfBirth.setStyle("-fx-background-color: lightgreen");
+                postalNumberField.setStyle("-fx-background-color: lightgreen");
+
+                Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+                alert1.setTitle("Potvrdi");
+                alert1.setHeaderText("Korisnik uspješno dodan.");
+                Optional<ButtonType> result1 = alert1.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    alert.close();
+                }
+                alert.close();
+            } else {
                 alert.close();
             }
-            alert.close();
-        } else {
-            alert.close();
+        }
+        else{
+            if(nameField.getText().equals("") || nameField.getText() == null) nameField.setStyle("-fx-background-color: lightpink");
+            else nameField.setStyle("-fx-background-color: lightgreen");
+
+            if(surnameField.getText().equals("") || surnameField.getText() == null) surnameField.setStyle("-fx-background-color: lightpink");
+            else surnameField.setStyle("-fx-background-color: lightgreen");
+
+            if(parentNameField.getText().equals("") || parentNameField.getText() == null) parentNameField.setStyle("-fx-background-color: lightpink");
+            else parentNameField.setStyle("-fx-background-color: lightgreen");
+
+            if(jmbgField.getText().equals("") || jmbgField.getText() == null) jmbgField.setStyle("-fx-background-color: lightpink");
+            else jmbgField.setStyle("-fx-background-color: lightgreen");
+
+            if(addressField.getText().equals("") || addressField.getText() == null) addressField.setStyle("-fx-background-color: lightpink");
+            else addressField.setStyle("-fx-background-color: lightgreen");
+
+            if(addressPlace.getValue() == null) addressPlace.setStyle("-fx-background-color: lightpink");
+            else addressPlace.setStyle("-fx-background-color: lightgreen");
+
+            if(!dateField.isPressed() || dateField.getValue().isAfter(ChronoLocalDate.from(LocalDateTime.now()))) dateField.setStyle("-fx-background-color: lightpink");
+            else {
+                dateField.getStyleClass().removeAll();
+                dateField.setStyle("-fx-background-color: lightgreen");
+            }
+
+            if(placeOfBirth.getValue() == null) placeOfBirth.setStyle("-fx-background-color: lightpink");
+            else placeOfBirth.setStyle("-fx-background-color: lightgreen");
+
+
+            if(postalNumberField.getText().equals("") || postalNumberField.getText() == null) postalNumberField.setStyle("-fx-background-color: lightpink");
+            else postalNumberField.setStyle("-fx-background-color: lightgreen");
+
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Nije moguće dodati korisnika!");
+            alert.setContentText("Niste popunili sva input polja!");
+
+            alert.showAndWait();
         }
     }
 
